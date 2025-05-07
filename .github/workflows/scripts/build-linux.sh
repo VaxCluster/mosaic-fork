7e032f3ac7f00274ccf70c06cf8b3eb213f6424e
VaxCluster <vaxcluster@vaxcluster.com>
2025-05-07 11:18:02 -0400

    switch to alma linux. build for ppc64le and s390x (lol)

--- .github/workflows/scripts/build-linux.sh
+++ .github/workflows/scripts/build-linux.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dependencies..."
 dnf install -y epel-release
-dnf config-manager --set-enabled ol8_codeready_builder
+#dnf config-manager --set-enabled ol8_codeready_builder
 dnf groupinstall -y 'Development Tools'
 dnf install libXt-devel libX11-devel libXmu-devel libXpm-devel motif-devel clang
 
