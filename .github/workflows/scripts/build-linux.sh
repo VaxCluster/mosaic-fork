d82b136b7aeb9395cd09b654f869093169a6cacf
VaxCluster <vaxcluster@vaxcluster.com>
2025-05-07 09:18:18 -0400

    forgot to install clang in github actions

--- .github/workflows/scripts/build-linux.sh
+++ .github/workflows/scripts/build-linux.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies..."
 dnf install -y epel-release
 dnf config-manager --set-enabled ol8_codeready_builder
 dnf groupinstall -y 'Development Tools'
-dnf install libXt-devel libX11-devel libXmu-devel libXpm-devel motif-devel
+dnf install libXt-devel libX11-devel libXmu-devel libXpm-devel motif-devel clang
 
 echo "Building Mosaic..."
 echo "Using compiler: $CC"
