3e3475b903dc3c3e32be66086738b10f944ef572
VaxCluster <vaxcluster@vaxcluster.com>
2025-05-15 15:44:37 -0400

    Workaround double free problems
    port of https://github.com/alandipert/ncsa-mosaic/commit/2b5f8dcd61c1ba594a5c80ec470d3bb6378f660d

--- src/main.c
+++ src/main.c
@@ -72,6 +72,9 @@
 #include <sys/utsname.h>
 #include <string.h>
 #include "compat.h"
+#ifdef __LINUX__
+#include <malloc.h>
+#endif
 
 /* swp */
 #define _KCMS_H_
@@ -224,6 +227,13 @@ main(int argc, char **argv, char **envp)
 
     /* Since we're doing lots of TCP, just ignore SIGPIPE altogether. */
     signal(SIGPIPE, SIG_IGN);
+
+#ifdef M_CHECK_ACTION
+  /* some code here has the habit of freeing pointers twice. The glibc
+     catches this and causes the program to abort. In order to make the
+     code somehow work, we turn aborting the program off. */
+  mallopt(M_CHECK_ACTION,5);
+#endif
 
     InitChildProcessor();
     MoCCIPreInitialize();
